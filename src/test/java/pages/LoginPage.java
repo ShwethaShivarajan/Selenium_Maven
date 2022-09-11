84961f5d906ca4cd1b6c0a0096a0ed1fc5d35faf
Shwetha Shivarajan <shwethashivarajan@gmail.com>
2022-09-11 11:19:59 +0530

    This is a testNG project

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,59 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+	WebDriver driver;
+
+	// *****************Objects/Web elements**************
+
+	@FindBy(linkText = "Log in")
+	WebElement LoginLink;
+
+	@FindBy(name = "user_login")
+	WebElement UserName;
+
+	@FindBy(id = "password")
+	WebElement Password;
+
+	@FindBy(className = "rememberMe")
+	WebElement Checkbox;
+
+	@FindBy(name = "btn_login")
+	WebElement LoginBtn;
+
+	// create the below constructor to use the base class driver and initialize the
+	// driver
+	public LoginPage(WebDriver basedriver) {
+		this.driver = basedriver;
+		PageFactory.initElements(basedriver, this); //pagefactory is a class -  initializes all page elements. FindBy doesn't work if this is not added
+	}
+
+	//page object model is a design pattern
+	// *********************Methods*************************
+
+	public void Login(String UsernameVal, String PwVal) {
+
+		// Step1. Click on the Login Link
+
+		LoginLink.click();
+
+		// Step2: Enter Username
+		UserName.sendKeys(UsernameVal);
+
+		// Step3: Enter Password
+		Password.sendKeys(PwVal);
+
+		// Step4 Click Remember Me checkbox
+		Checkbox.click();
+
+		// Step5: Click on Login Button
+		LoginBtn.click();
+	}
+
+}
---
+++ src/test/java/testcases/SimplilearnLoginTest.java
@@ -0,0 +1,55 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+
+public class SimplilearnLoginTest extends BaseClass {
+
+	@Test
+	@Parameters({"uname","pwd"})
+	public void Test1(String UserName, String Password) {
+
+		// passing the base class driver in the parameter to loginpage method
+		LoginPage lp = new LoginPage(driver);
+
+		lp.Login(UserName, Password);
+
+		// Step6: Validate the error message on screen
+		WebElement Error = driver.findElement(By.id("msg_box"));
+
+		String ActError = Error.getText();
+		String ExpError = "The email or password you have entered is invalid.";
+
+		Assert.assertEquals(ActError, ExpError);
+		Assert.assertTrue(Error.isDisplayed());
+	}
+
+	@Test
+	public void Test2() {
+		
+		//negative TC
+		LoginPage lp = new LoginPage(driver);
+		lp.Login("abc@xyz.com", "Abcd@1233");
+
+	}
+
+	
+	@Test
+	public void Test3() {
+		String UserName = sheet.getRow(1).getCell(0).getStringCellValue();
+        String Password = sheet.getRow(1).getCell(1).getStringCellValue();
+		
+		LoginPage lp = new LoginPage(driver);
+		lp.Login(UserName, Password);
+
+	}
+}
